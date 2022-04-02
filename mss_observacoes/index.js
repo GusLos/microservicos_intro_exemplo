332f85298894716d15c50e408d3fdf8190061ff6
Gustavo <gu.losada@hotmail.com>
2022-04-02 11:59:24 -0300

    Final intervalo

--- mss_observacoes/index.js
+++ mss_observacoes/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const {v4: uuidv4} = require('uuid')
 const app = express()
 app.use(express.json())
+
+const observacoesPorLembreteId = {}
 
 //POST
 //host:porta/lembrete/123456/observacoes
 app.post('/lembretes/:id/observacoes', (req, res) => {
-    //
+    const idObs = uuidv4()
+    //const texto = req.body      //funciona por causa da linha: app.use(express.json())    
+    const { texto } = req.body      //desestruturação - mesma coisa q o de cima 
+    const observacoesDoLembrete = observacoesPorLembreteId [req.params.id] ||/*ou*/  []     //verifica se é undefined, se for acontece o outro lado
+    observacoesDoLembrete.push({id: idObs, texto: texto})// ou .push({id: idObs, texto})
+    observacoesPorLembreteId[req.params.id] = observacoesDoLembrete
+    res.status(201).send(observacoesDoLembrete)
 })
 
 
